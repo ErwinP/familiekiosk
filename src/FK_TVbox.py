fceba756e01604e702614fe4c7674a717a14a3cc
Erwin Pannecoucke <erwin.pannecoucke@gmail.com>
2019-04-11 11:57:45 +0200

    merge radomization component into the exif-based transposition

--- src/FK_TVbox.py
+++ src/FK_TVbox.py
@@ -51,6 +51,9 @@ gi.require_version('GstVideo', '1.0')
 gi.require_version('GdkX11', '3.0')
 from gi.repository import Gst, GObject, GdkX11, GstVideo
 
+import random
+from random import shuffle
+
 
 BASE_PIC_PATH = os.path.abspath(os.path.dirname(sys.argv[0])) + '/pics/test/'
 IMAGES = os.path.join(BASE_PIC_PATH, '*.jpg')
@@ -80,6 +83,8 @@ class TVbox():
         self.showvideonr = 0
         self.showaudionr = 0
         self.len_img_list = 0
+        self.new_images = -2
+        self.first_photo_already_shown = 0
         self.len_vid_list = 0
         self.len_aud_list = 0
         self.list_of_img = []
@@ -230,18 +235,33 @@ class TVbox():
 
     
         list_of_files = [x for x in list_of_files if x[-9:] != "_comp.jpg"]
-        if self.len_img_list != len(list_of_files):
-            #new image arrived, show it
-            self.showimagenr = 0
-        self.len_img_list = len(list_of_files)
-        
-        if self.most_recent_mode():
-            self.list_of_img = list_of_files[:MAX_JPG]
-        elif (RANDOMIZE_PHOTOS == True):
-            import random
-            from random import shuffle
+        if self.len_img_list != len(list_of_files) and self.new_images != -2 :
+            #New images have arived, show the first ones as determined in the config
+            print("Showing the newly-arrived images")
+            self.new_images =  len(list_of_files) - self.len_img_list
             self.list_of_img = list_of_files
-            shuffle(self.list_of_img)
+            if self.first_photo_already_shown == 0 :
+                #Reset showimagenr only the first time we're in this loop of new photo's
+                self.showimagenr = 0
+                self.first_photo_already_shown = 1
+            if self.new_images == self.showimagenr or self.showimagenr == MAX_JPG :
+                #When all new images have been shown or if we're at the maximum, 
+                #get out of this condition and prepair to detect for a new load
+                print("all new images have been shown")
+                self.len_img_list = len(list_of_files)
+                self.new_images = -1 
+                self.first_photo_already_shown = 0
+        else:
+            #if here, it's because the pi was booted for the first time
+            self.list_of_img = list_of_files
+            self.len_img_list = len(list_of_files)
+
+        if self.most_recent_mode():
+            #print (self.most_recent_mode)
+            self.list_of_img = list_of_files[:MAX_JPG]
+        elif (RANDOMIZE_PHOTOS == True) :
+            #do not rebuild the list of images while in random mode
+            self.list_of_img =  self.list_of_img
         else:
             self.list_of_img = list_of_files
         #print (self.list_of_img)
@@ -323,6 +343,12 @@ class TVbox():
         if self.currentimage != self.showimagenr:
             # we regenerate list of images to have latest present
             self.listimages()
+            # Only consider shuffling if there are no new images to be shown
+            if self.showimagenr  > self.new_images and self.new_images < 0 and RANDOMIZE_PHOTOS == True : 
+                 shuffle(self.list_of_img)
+                 self.new_images = 0
+                 self.showimagenr = 0
+                 print ("Shuffled the list, new_images is now ", self.new_images)
             # now show the next image
             if len(self.list_of_img) == 0 :
                 #no images yet, show dummy
@@ -366,10 +392,8 @@ class TVbox():
             Apply Image.transpose to ensure 0th row of pixels is at the visual
             top of the image, and 0th column is the visual left-hand side.
             Return the original image if unable to determine the orientation.
-
             As per CIPA DC-008-2012, the orientation field contains an integer,
             1 through 8. Other values are reserved.
-
             Thanks to Roman Odaisky and others:
             https://stackoverflow.com/questions/4228530/pil-thumbnail-is-rotating-my-image/
         """
